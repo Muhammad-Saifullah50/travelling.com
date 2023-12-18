20fdf0cc53a5afae5eb43b1eed29adc1a601d1b4
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-18 22:01:24 +0500

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (request: NextRequest) => {
+
+    let pathname = request.nextUrl.pathname
+    
+    let token = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token')
+
+    const publicPath = pathname === '/' || pathname === '/login' || pathname === '/register' || pathname === '/search' 
+
+    if (!publicPath && !token) {
+        const response = NextResponse.redirect(new URL('/login', request.nextUrl))
+        return response
+
+    }
+    if (token && pathname === '/signin') {
+        const response = NextResponse.redirect(new URL('/', request.nextUrl))
+        return response
+    }
+    if (token) {
+        const response = NextResponse.next()
+        return response
+    }
+}
+
+
+
+export const config =
+{
+    matcher: [
+        '/',
+        '/rent',
+        '/trips',
+        '/favourites',
+        '/reservations',
+        '/properties',
+        '/edit-listing/:path*',
+        '/listings/:path*',
+        '/login',
+        '/register',
+        '/search'
+    ]
+}
