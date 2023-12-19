944a0fe45048b5c31b546b6eb639d309fd124034
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-19 22:00:52 +0500

    added hfull to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -50,7 +50,7 @@ const Modal = ({ isOpen, onClose, onSubmit, title, body, footer, actionLabel, di
         <div className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 '>
             <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full md:h-auto'>
                 <div className={clsx("h-full")}>
-                    <div className=' h-full md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none mt-2'>
+                    <div className=' h-full border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none mt-2'>
                         {/* Header */}
                         <div className='flex items-center rounded-t p-6 justify-center relative border-b-[1px]'>
 
