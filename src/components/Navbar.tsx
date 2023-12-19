b123b8c5301866f1aa3bbb3584542704e2fbef2c
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-19 12:05:11 +0500

    completed app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,13 +32,13 @@ const Navbar = async () => {
               width={50}
               height={50}
             />
-            <h1 className='max-md:hidden text-2xl font-extrabold text-gray-900'>Travelling.com</h1>
+            <h1 className='max-sm:hidden text-2xl font-extrabold text-gray-900'>Travelling.com</h1>
           </div>
         </Link>
 
-        <SearchCard/>
+        <SearchCard />
 
-        
+
         <div className='flex gap-4 items-center'>
           <Link href={'/rent'}>{session && <h2 className='max-md:hidden text-sm font-bold'>Register your place</h2>} </Link>
 
@@ -48,32 +48,47 @@ const Navbar = async () => {
                 <Menu className='w-5 h-5 text-gray-700' />
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuLabel>My Profile</DropdownMenuLabel>
                 <DropdownMenuSeparator />
 
                 {!session ? (
                   <>
-                    <DropdownMenuItem>
-                      <Link href={'/register'}>Register</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Link href={'/login'}>Login</Link>
-                    </DropdownMenuItem>
+                    <Link href={'/register'}>
+                      <DropdownMenuItem>
+                        Register
+                      </DropdownMenuItem>
+                    </Link>
+
+                    <Link href={'/login'}>
+                      <DropdownMenuItem>
+                        Log in
+                      </DropdownMenuItem>
+                    </Link>
                   </>) : (<>
-                    <DropdownMenuItem>
-                      <Link href={'/trips'}>My Trips</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Link href={'/favourites'}>My Favourites</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Link href={'/reservations'}>My Reservations</Link>
-                    </DropdownMenuItem> <DropdownMenuItem>
-                      <Link href={'/properties'}>My Properties</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className='hidden md:block'>
-                      <Link href={'/'}>Register your place</Link>
-                    </DropdownMenuItem>
+                    <Link href={'/trips'}>
+                      <DropdownMenuItem>
+                        My Trips
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href={'/favourites'}>
+                      <DropdownMenuItem>
+                        My Favourites
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href={'/reservations'}>
+                      <DropdownMenuItem>
+                        My Reservations
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href={'/properties'}>
+                      <DropdownMenuItem>
+                        My Properties
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href={'/rent'}>
+                      <DropdownMenuItem>
+                        Register your place
+                      </DropdownMenuItem>
+                    </Link>
                     <DropdownMenuSeparator />
 
                     <DropdownMenuItem className='font-semibold'><SignOutBtn /></DropdownMenuItem>
