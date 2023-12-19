a4b480c08dcfffe2134a4975c23494d5afb853ad
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-19 09:01:06 +0500

    commented search modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({ children, authmodal, rentmodal, editm
           {authmodal}
           {rentmodal}
           {editmodal}
-          {searchmodal}
+          {/* {searchmodal} */}
           <Footer/>
         </body>
       </AuthSessionProvider>
--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -21,149 +21,149 @@ enum STEPS {
 }
 
 const SearchModal = () => {
-    // const router = useRouter();
-    // const [step, setStep] = useState(STEPS.Location)
-    // const [guestCount, setGuestCount] = useState(1);
-    // const [roomCount, setRoomCount] = useState(1);
-    // const [bathroomCount, setBathroomCount] = useState(1);
-    // const [location, setLocation] = useState<CountrySelectValue>();
-    // const [category, setCategory] = useState(categories[0].label);
-    // const [dateRange, setDateRange] = useState<Range>({
-    //     startDate: new Date(),
-    //     endDate: new Date(),
-    //     key: 'selection'
-    // });
+    const router = useRouter();
+    const [step, setStep] = useState(STEPS.Location)
+    const [guestCount, setGuestCount] = useState(1);
+    const [roomCount, setRoomCount] = useState(1);
+    const [bathroomCount, setBathroomCount] = useState(1);
+    const [location, setLocation] = useState<CountrySelectValue>();
+    const [category, setCategory] = useState(categories[0].label);
+    const [dateRange, setDateRange] = useState<Range>({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection'
+    });
 
-    // const formattedStartDate = formatISO(Number(dateRange.startDate));
-    // const formattedEndDate = formatISO(Number(dateRange.endDate));
+    const formattedStartDate = formatISO(Number(dateRange.startDate));
+    const formattedEndDate = formatISO(Number(dateRange.endDate));
 
-    // const onBack = useCallback(() => {
-    //     setStep((value) => value - 1);
-    // }, [setStep]);
+    const onBack = useCallback(() => {
+        setStep((value) => value - 1);
+    }, [setStep]);
 
-    // const onNext = useCallback(() => {
-    //     setStep((value) => value + 1);
-    // }, [setStep]);
+    const onNext = useCallback(() => {
+        setStep((value) => value + 1);
+    }, [setStep]);
 
-    // const onSubmit = async () => {
-    //     if (step !== STEPS.Info) return onNext();
+    const onSubmit = async () => {
+        if (step !== STEPS.Info) return onNext();
 
-    //     router.push(`/?startDate=${formattedStartDate}&endDate=${formattedEndDate}&location=${location?.value}&guestCount=${guestCount}&roomCount=${roomCount}&bathroomCount=${bathroomCount}&category=${category}`);
+        router.push(`/?startDate=${formattedStartDate}&endDate=${formattedEndDate}&location=${location?.value}&guestCount=${guestCount}&roomCount=${roomCount}&bathroomCount=${bathroomCount}&category=${category}`);
 
-    //     router.refresh();
-    // }
-    // const actionLabel = () => {
-    //     if (step === STEPS.Info) {
-    //         return 'Search'
-    //     }
-    //     return 'Next'
-    // }
+        router.refresh();
+    }
+    const actionLabel = () => {
+        if (step === STEPS.Info) {
+            return 'Search'
+        }
+        return 'Next'
+    }
 
-    // const secondaryActionLabel = () => {
-    //     if (step === STEPS.Location) {
-    //         return undefined
-    //     }
-    //     return 'Back'
-    // }
+    const secondaryActionLabel = () => {
+        if (step === STEPS.Location) {
+            return undefined
+        }
+        return 'Back'
+    }
 
 
-    // const Map = dynamic(() => import('../Map'), {
-    //     ssr: false
-    // });
+    const Map = dynamic(() => import('../Map'), {
+        ssr: false
+    });
 
-    // let bodyContent = (
-    //     <>
-    //         <Heading
-    //             title="Where do you want to go?"
-    //             subtitle="Help us find the perfect place!"
-    //         />
+    let bodyContent = (
+        <>
+            <Heading
+                title="Where do you want to go?"
+                subtitle="Help us find the perfect place!"
+            />
 
-    //         <CountrySelect
-    //             onChange={(value) => setLocation(value as CountrySelectValue)}
-    //             value={location}
-    //         />
-    //         <Map center={location?.latlng} />
-    //     </>
-    // )
+            <CountrySelect
+                onChange={(value) => setLocation(value as CountrySelectValue)}
+                value={location}
+            />
+            <Map center={location?.latlng} />
+        </>
+    )
 
-    // if (step === STEPS.Category) {
-    //     bodyContent = (<>
+    if (step === STEPS.Category) {
+        bodyContent = (<>
 
-    //         <Heading
-    //             title='What sort of place would you like to visit?'
-    //             subtitle='Get recommendations near your chosen category'
-    //         />
+            <Heading
+                title='What sort of place would you like to visit?'
+                subtitle='Get recommendations near your chosen category'
+            />
 
-    //         <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
-    //             {categories.map((item) => (
-    //                 <CategoryInput
-    //                     key={item.label}
-    //                     selected={item.label === category}
-    //                     label={item.label}
-    //                     icon={item.icon}
-    //                     onClick={(category) => setCategory(category)}
-    //                 />
-    //             ))}
-    //         </div>
-    //     </>)
-    // }
-    // if (step === STEPS.Date) {
-    //     bodyContent = (
-    //         <div className='flex flex-col'>
-    //             <Heading
-    //                 title='When do you plan to go?'
-    //                 subtitle='Make sure everyone is free!'
-    //             />
-    //             <Calendar
-    //                 value={dateRange}
-    //                 //@ts-ignore
-    //                 onChange={(value) => setDateRange(value.selection)}
-    //             />
-    //         </div>
-    //     )
-    // }
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
+                {categories.map((item) => (
+                    <CategoryInput
+                        key={item.label}
+                        selected={item.label === category}
+                        label={item.label}
+                        icon={item.icon}
+                        onClick={(category) => setCategory(category)}
+                    />
+                ))}
+            </div>
+        </>)
+    }
+    if (step === STEPS.Date) {
+        bodyContent = (
+            <div className='flex flex-col'>
+                <Heading
+                    title='When do you plan to go?'
+                    subtitle='Make sure everyone is free!'
+                />
+                <Calendar
+                    value={dateRange}
+                    //@ts-ignore
+                    onChange={(value) => setDateRange(value.selection)}
+                />
+            </div>
+        )
+    }
 
-    // if (step === STEPS.Info) {
-    //     bodyContent = (
-    //         <div className='flex flex-col'>
-    //             <Heading
-    //                 title='Share some basics about your destination'
-    //                 subtitle='What amenities do you need?'
-    //             />
+    if (step === STEPS.Info) {
+        bodyContent = (
+            <div className='flex flex-col'>
+                <Heading
+                    title='Share some basics about your destination'
+                    subtitle='What amenities do you need?'
+                />
 
-    //             <Counter
-    //                 title='Guests'
-    //                 subtitle='How many guests are coming?'
-    //                 value={guestCount}
-    //                 onChange={(value) => setGuestCount(value)}
-    //             />
-    //             <Counter
-    //                 title='Rooms'
-    //                 subtitle='How many rooms will you need?'
-    //                 value={roomCount}
-    //                 onChange={(value) => setRoomCount(value)}
-    //             />
-    //             <Counter
-    //                 title='Bathrooms'
-    //                 subtitle='How many bathrooms do you need?'
-    //                 value={bathroomCount}
-    //                 onChange={(value) => setBathroomCount(value)}
-    //             />
-    //         </div>
-    //     )
-    // }
+                <Counter
+                    title='Guests'
+                    subtitle='How many guests are coming?'
+                    value={guestCount}
+                    onChange={(value) => setGuestCount(value)}
+                />
+                <Counter
+                    title='Rooms'
+                    subtitle='How many rooms will you need?'
+                    value={roomCount}
+                    onChange={(value) => setRoomCount(value)}
+                />
+                <Counter
+                    title='Bathrooms'
+                    subtitle='How many bathrooms do you need?'
+                    value={bathroomCount}
+                    onChange={(value) => setBathroomCount(value)}
+                />
+            </div>
+        )
+    }
 
 
     return (
         <Modal
             isOpen
             title='Search by adding filters'
-            // onClose={() => router.back()}
-            // actionLabel={actionLabel()}
-            // secondaryActionLabel={secondaryActionLabel()}
-            // secondaryAction={step === STEPS.Location ? undefined : onBack}
-            // onSubmit={onSubmit}
-            // body={bodyContent}
+            onClose={() => router.back()}
+            actionLabel={actionLabel()}
+            secondaryActionLabel={secondaryActionLabel()}
+            secondaryAction={step === STEPS.Location ? undefined : onBack}
+            onSubmit={onSubmit}
+            body={bodyContent}
         />
     )
 }
