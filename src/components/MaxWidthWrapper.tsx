860dc44c110f24f7a32cc8f3d3bc20a176f23a59
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-19 09:22:14 +0500

    adde default fiel in slot

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,7 +1,7 @@
 
 const MaxWidthWrapper = ({ children, className }: { children: React.ReactNode, className?: string }) => {
     return (
-        <div className={`w-full mx-auto max-w-7xl px-5 sm:px-6  ${className}`}>
+        <div className={`w-full mx-auto max-w-screen-xl px-5 sm:px-6 ${className}`}>
             {children}
         </div>
     )
