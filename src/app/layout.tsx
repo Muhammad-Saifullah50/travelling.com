f109e00222972e33c81cad60e64aa9fb38670b25
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-19 09:05:34 +0500

    removed search modal slot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ interface RootLayoutProps {
   authmodal: React.ReactNode
   rentmodal: React.ReactNode
   editmodal: React.ReactNode
-  searchmodal: React.ReactNode
+  // searchmodal: React.ReactNode
 }
 
-export default async function RootLayout({ children, authmodal, rentmodal, editmodal, searchmodal }: RootLayoutProps) {
+export default async function RootLayout({ children, authmodal, rentmodal, editmodal }: RootLayoutProps) {
 
   return (
     <html lang="en">
